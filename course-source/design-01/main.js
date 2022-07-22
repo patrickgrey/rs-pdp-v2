819eb0f282464f91beac2bf30aa1871eedb22bc8
patrickgrey <websemantic@yahoo.co.uk>
2022-07-22 11:46:22 +0100

    Date picker added

--- course-source/design-01/main.js
+++ course-source/design-01/main.js
@@ -1,9 +1,9 @@
-
+import { DuetDatePicker } from "@duetds/date-picker/custom-element";
 
 var pageModule = (function () {
   var module = {};
   module.init = function () {
-
+    customElements.define("duet-date-picker", DuetDatePicker);
   };
   return module;
 })();
